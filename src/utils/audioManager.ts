a1ae280a4054aba0caff45ebb0c9d12245bd5f6c
Jason <jasontorres585@icloud.com>
2025-08-28 14:21:48 -0400

    Fix malformed base64 audio string

--- src/utils/audioManager.ts
+++ src/utils/audioManager.ts
@@ -166,7 +166,7 @@ class AudioManager {
     const frequencies = fallbackFrequencies[soundName] || [440];
     
     const howl = new Howl({
-      src: ['data:audio/wav;base64,UklGRnoGAABXQVZFZm10IBAAAAABAAEAQB8AAEAfAAABAAgAZGF0YQoGAACBhYqFbF1fdJivrJBhNjVgodDbq2EcBj+a2/LDciUFLIHO8tiJNwgZaLvt559NEAxQp+PwtmMcBjiR1/LMeSwFJHfH8N2QQAoUXrTp66hVFApGn+DyvmwhBSuBzvLZiTYIG2m98OScTgwOUarm7blmGgU7k9n1unEiBC13yO/eizEIHWq+8+OWT'],
+      src: ['data:audio/wav;base64,UklGRigAAABXQVZFZm10IBAAAAAAQESAAEAEgAABAAgAZGF0YQQAAAAAAA=='],
       volume: 0.3
     });
 
